154e95342ad53148fbf18259421b5114d39d2438
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-03-11 02:18:07 +0000

    Update Enchant_Notifier_Main.lua

--- Project_Folder/Deepwoken/Enchant_Notifier_Main.lua
+++ Project_Folder/Deepwoken/Enchant_Notifier_Main.lua
@@ -1,4 +1,208 @@
-local Script = game:HttpGet("https://raw.githubusercontent.com/R00T66/Main/main/Project_Folder/Deepwoken/Enchant_Notifier")
-local Encode,Decode,Write = loadstring(game:HttpGet("https://raw.githubusercontent.com/R00T66/raw/main/Custom_Bytecode.lua"))()
+--// ENCHANT NOTIFIER \\--
+--// MADE BY METACALLED \\--
 
-loadstring(Decode(Script))()
+local Deepwoken_Locations = {
+ ["OVERWORLD"] = 6032399813,
+ ["DEPTHS"] = 5735553160
+}
+
+local Library_Status = {
+ ["Base64"] = false,
+ ["Notifications"] = false,
+ ["Events"] = false
+}
+
+-- LOADING --
+
+local GameLoaded = game.IsLoaded
+
+if not GameLoaded then
+   game.IsLoaded:Wait()
+end
+
+-- SERVICES --
+
+local HttpService = game:GetService("HttpService")
+local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+
+-- LIBARIES LOADING --
+
+local Directory = "https://raw.githubusercontent.com/R00T66/Main/main/Project_Folder/Deepwoken/Dependancies/"
+
+local SuccessBase64,ReturnsBase64,Base64_Library = pcall(game.HttpGet, game, Directory .. "Base64_Library")
+local SuccessNotification,ReturnsNotification,Notification_Library = pcall(game.HttpGet, game, Directory .. "Notifications_Library")
+local SuccessEvents,ReturnsEvents,Events_Library = pcall(game.HttpGet, game, Directory .. "Events_Library")
+
+-- LIBRARY FUNCTIONS --
+
+local Success = function(Type, Misc)
+   if Type == "Library" then
+      Library_Status[Misc] = true
+   end 
+end
+
+local Failed = function(Type, Misc)
+   if Type == "Library" then
+      Library_Status[Misc] = false
+
+      game:GetService("Players").LocalPlayer:Kick("The following library ran into an error: '" .. Misc .. "'")
+   end    
+end
+
+-- LIBARIES INIT --
+
+if SuccessBase64 then
+   Success("Library", "Base64")
+ 
+   Base64_Library = loadstring(ReturnsBase64)()
+else  
+   Failed("Library", "Base64")
+
+   return
+end
+
+if SuccessNotification then
+   Success("Library", "Notifications")
+
+   Notification_Library = loadstring(ReturnsNotification)()
+else
+   Failed("Library", "Notifications")
+
+   return
+end
+
+if SuccessEvents then
+   Success("Library", "Events")
+
+   Events_Library = loadstring(ReturnsEvents)()
+else
+   Failed("Library", "Events")
+
+   return
+end
+
+-- MAIN --
+
+local Desert_Enchant = function(String)
+   local STRING_FORMATTED = String:gsub("(.*)%@.*$","%1")
+
+   return STRING_FORMATTED
+end
+
+local Desert_Name = function(String)
+   local STRING_FORMATTED = String:gsub("(.*)%$.*$","%1")
+
+   return STRING_FORMATTED
+end
+
+local Desert_ID = function(String)
+   local STRING_FORMATTED = String:gsub("(*.)%$*(.*)","%1")
+
+   return STRING_FORMATTED
+end
+
+local Template_String = function(Enchant, Tool, SoulBound, Player)
+   
+   local String = "Enchant: ENCHANT_NAME_THING\nTool: TOOL_NAME_THING\nSOULBOUND: SOUL_BOUND_THING\nPlayer: PLAYER_NAME_THING"
+
+   String = String:gsub("ENCHANT_NAME_THING", Enchant)
+   String = String:gsub("TOOL_NAME_THING", Tool)
+   String = String:gsub("SOUL_BOUND_THING", tostring(SoulBound))
+   String = String:gsub("PLAYER_NAME_THING", Player)
+
+   return String
+end
+
+local CheckTool = function(Object, PlayerName)
+   if Object:IsA("Tool") then      
+      if Object:FindFirstChild("WeaponData") then
+         
+         local WeaponValue = Object:FindFirstChild("WeaponData").Value; WeaponValue = Desert_Enchant(Base64_Library:Decode(WeaponValue))
+         local WeaponData = Events_Library:JSONDecode(WeaponValue)
+
+         if WeaponData.Enchant ~= nil then
+            if not WeaponData.SoulBound then
+ 
+                local Enchant_String = tostring(WeaponData.Enchant)
+                local Enchant_Tool = tostring(Desert_Name(Object.Name) .. " ( " .. string.split(Object.Name, "$")[2] .. ")")
+                local SoulBound = false
+
+                Notifications_Library:SendNotification("Enchant Notifier", Template_String(Enchant_String, Enchant_Tool, SoulBound, PlayerName), UDim2.new(.5, -650, .5, 0), 9061754547)
+            else
+                local Enchant_String = tostring(WeaponData.Enchant)
+                local Enchant_Tool = tostring(Desert_Name(Object.Name)  .. " ( $" .. string.split(Object.Name, "$")[2] .. " )")
+                local SoulBound = true
+                
+                print(Desert_ID(Object.Name))
+                
+                Notification_Library:SendNotification("Enchant Notifier", Template_String(Enchant_String, Enchant_Tool, SoulBound, PlayerName), UDim2.new(.5, -650, .5, 0), 8904888220)
+            end
+         end
+      end
+   end
+end
+
+Events_Library:PlayerAdded():Connect(function(self)
+    repeat wait() until self:FindFirstChild("Backpack")
+
+    local Backpack = self:FindFirstChild("Backpack")
+   
+    for i, tool in pairs(Backpack:GetChildren()) do
+       if self.UserId ~= Players.LocalPlayer.UserId then CheckTool(tool, self.Name) end
+    end
+
+    if self.UserId ~= Players.LocalPlayer.UserId then Backpack.ChildAdded:Connect(function(tool)
+        if self ~= nil then
+           coroutine.resume(
+               coroutine.create(function()
+                   local yield = 0;
+                   
+                   repeat wait() until self:FindFirstChild("WeaponData") or yield > 50 
+                   
+                   CheckTool(tool, self.Name)
+               end)
+           )
+        end
+    end) end
+end)
+
+for i, self in pairs(Players:GetPlayers()) do
+    
+    repeat wait() until self:FindFirstChild("Backpack")
+
+    local Backpack = self:FindFirstChild("Backpack")
+   
+    for i, tool in pairs(Backpack:GetChildren()) do
+       if self.UserId ~= Players.LocalPlayer.UserId then CheckTool(tool, self.Name) end
+    end
+
+    if self.UserId ~= Players.LocalPlayer.UserId then Backpack.ChildAdded:Connect(function(tool)
+        if self ~= nil then
+           coroutine.resume(
+               coroutine.create(function()
+                   local yield = 0;
+                   
+                   repeat wait() until self:FindFirstChild("WeaponData") or yield > 50 
+                   
+                   CheckTool(tool, self.Name)
+               end)
+           )
+        end
+    end) end
+end
+
+Notification_Library:SendNotification(
+ "Enchant Notifier", 
+ "SUCCESSFULLY LOADED!", 
+ UDim2.new(.5, -650, .5, 0), 
+ 8904888220
+)
+
+local Blacklist = {1}
+
+if table.find(Blacklist, game.Players.LocalPlayer.UserId) then
+   game.Players.LocalPlayer:Kick("[YOU HAVE BEEN BLACKLISTED]")
+
+   return
+end

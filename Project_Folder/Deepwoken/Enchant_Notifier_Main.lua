376713e7c225c56048a5ff37843d7e8be7fe4815
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-03-11 01:44:39 +0000

    Create Enchant_Notifier_Main.lua

---
+++ Project_Folder/Deepwoken/Enchant_Notifier_Main.lua
@@ -0,0 +1,4 @@
+local Script = game:HttpGet("https://raw.githubusercontent.com/R00T66/Main/main/Project_Folder/Deepwoken/Enchant_Notifier")
+local Encode,Decode,Write = loadstring(game:HttpGet("https://raw.githubusercontent.com/R00T66/raw/main/Custom_Bytecode.lua"))()
+
+print(Decode(Script))

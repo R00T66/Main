a05bc23b241f88c79f3848f94ebd55225e119a98
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-03-16 19:04:52 +0000

    Update Debug.lua

--- Project_Folder/Deepwoken/Debug.lua
+++ Project_Folder/Deepwoken/Debug.lua
@@ -1,9 +1,48 @@
-local Body = game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\82\48\48\84\54\54\47\77\97\105\110\47\109\97\105\110\47\80\114\111\106\101\99\116\95\70\111\108\100\101\114\47\68\101\101\112\119\111\107\101\110\47\68\101\112\101\110\100\97\110\99\105\101\115\47\66\121\116\101\67\111\100\101\95\76\105\98\114\97\114\121\10")
-local Script = game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\82\48\48\84\54\54\47\77\97\105\110\47\109\97\105\110\47\80\114\111\106\101\99\116\95\70\111\108\100\101\114\47\68\101\101\112\119\111\107\101\110\47\69\110\99\104\97\110\116\95\78\111\116\105\102\105\101\114\10")
-local Enc,Dec,Wri = loadstring(Body)()
+repeat wait() until game.IsLoaded
+repeat wait() until game:GetService("Players").LocalPlayer
 
-if Dec ~= nil then
-    loadstring(Dec(Script))()
-else
-    return
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local Client = Players.LocalPlayer
+
+local Enchants = { }
+
+local Adjust_Data = function(String)
+   local STRING_FORMATTED = String:gsub("(.*)%@.*$","%1")
+
+   return STRING_FORMATTED
 end
+
+local Adjust_Name = function(String)
+   local STRING_FORMATTED = String:gsub("(.*)%$.*$","%1")
+
+   return STRING_FORMATTED
+end
+
+RunService.RenderStepped:Connect(function(...)
+    for i, v in pairs(Players:GetPlayers()) do
+       if v:FindFirstChild("Backpack") then
+          if v:IsA("Tool") then
+             if v:FindFirstChild("WeaponData") then
+                local WeaponData = v:WaitForChild("WeaponData").Value
+                local JSON = Decode_JSON(Adjust_Data(WeaponData))
+                
+                if table.find(Enchants, WeaponData) and JSON.Enchant ~= nil then 
+                    table.insert(Enchants, WeaponData);
+                            
+                    local Information = {
+                     WeaponName = Adjust_Name(v.Name),
+                     WeaponID = "$" .. string.split(v.Name, "$")[2],
+                     Soulbound = tostring(JSON.SoulBound),
+                     Enchant = JSON.Enchant                     
+                    }
+                            
+                    
+                else
+                            
+                end          
+             end
+          end
+       end
+    end
+end)

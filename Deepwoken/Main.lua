14fe26d736d02363b7a02ed38b52079b97ea60c5
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-03-18 07:30:42 +0000

    Update Main.lua

--- Deepwoken/Main.lua
+++ Deepwoken/Main.lua
@@ -339,7 +339,7 @@ end
 coroutine.resume(
     coroutine.create(
         function(...)
-            while wait(2) do
+            while wait(.5) do
                 for i, v in pairs(Players:GetPlayers()) do
                     if v:FindFirstChild("Backpack") then
                         if v ~= Players.LocalPlayer then

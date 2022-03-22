21477b636d6b274ea92dd2df17d45ad67e4beede
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-03-22 11:06:42 +0000

    Delete Project_Folder directory

--- Project_Folder/Deepwoken/Enchant_Notifier_Main.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/R00T66/Main/main/Deepwoken/Main.lua"))()
--- Project_Folder/readme.md
+++
@@ -1,1 +0,0 @@
-Things here are projects - not regularly updated, but may be updated every now and again.

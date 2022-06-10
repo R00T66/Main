163e55d85dcdd53ee7c4169970910dea910a0cc1
R00T66 <92213007+R00T66@users.noreply.github.com>
2022-06-10 14:46:43 +0100

    Update Main.lua

--- EL2/Main.lua
+++ EL2/Main.lua
@@ -99,6 +99,7 @@ local ModAlert = function(Name, Role)
      Text = Name .. " [ " .. Role .. " ]",
      Button1 = "OK",
      Button2 = "HOP",
+     Duration = 35235325
      Callback = Func
    })
 end
